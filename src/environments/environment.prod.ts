9557643b5cb2b6f35baa0beb08ed6ea695675268
InTeRNaL <kezik.1992@gmail.com>
2021-10-07 13:40:13 +0300

    add gh pages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,12 @@
 export const environment = {
   production: true,
+  firebase: {
+    apiKey: "AIzaSyDx0zPyNhw9e18sT0AhslnDnsZtMphmIcc",
+    authDomain: "fir-authapp-8b012.firebaseapp.com",
+    projectId: "fir-authapp-8b012",
+    storageBucket: "fir-authapp-8b012.appspot.com",
+    messagingSenderId: "1080859034886",
+    appId: "1:1080859034886:web:04db6063c51f479dfaca87"
+  },
   apiUrl: 'https://api.github.com/'
 };
